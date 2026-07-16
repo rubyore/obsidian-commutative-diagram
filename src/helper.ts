0b56bc09185dc8a6d1bfce1d27ab1e2fcea2048a
rubyore <kazuharu.nagamura@gmail.com>
2026-07-16 18:00:13 +0900

    Moved `computeIntersection` to `helpers.ts`

--- src/helper.ts
+++ src/helper.ts
@@ -21,13 +21,29 @@ export async function getSize(el: HTMLElement): Promise<DOMRect> {
     }
 }
 
-export async function getBiggerSize(el: HTMLElement, old: DOMRect): Promise<DOMRect> {
-    while (true) {
-        const rect = el.getBoundingClientRect();
-        if (rect.width > old.width || rect.height > old.height) {
-            return rect;
-        }
+export function computeIntersections(a: DOMRect, b: DOMRect) {
+  const ax = a.x + a.width / 2;
+  const ay = a.y + a.height / 2;
+  const bx = b.x + b.width / 2;
+  const by = b.y + b.height / 2;
 
-        await new Promise(resolve => setTimeout(resolve, 0));
-    }
+  const dx = bx - ax;
+  const dy = by - ay;
+
+  function intersect(cx: number, cy: number, w: number, h: number, dx: number, dy: number) {
+    const t = Math.min(
+      dx === 0 ? Infinity : (w / 2) / Math.abs(dx),
+      dy === 0 ? Infinity : (h / 2) / Math.abs(dy)
+    );
+
+    return {
+      x: cx + dx * t,
+      y: cy + dy * t,
+    };
+  }
+
+  return {
+    from: intersect(ax, ay, a.width, a.height, dx, dy),
+    to: intersect(bx, by, b.width, b.height, -dx, -dy),
+  };
 }
--- src/render.ts
+++ src/render.ts
@@ -1,4 +1,5 @@
 import { finishRenderMath, renderMath } from "obsidian";
+import { computeIntersections } from "./helper";
 
 const textColor = document.body.getCssPropertyValue("--text-normal");
 
@@ -112,31 +113,4 @@ export async function renderArrow(start: DOMRect, end: DOMRect, el: HTMLElement,
   div.appendChild(label)
 
   return [path, head, FO];
-}
-
-function computeIntersections(a: DOMRect, b: DOMRect) {
-  const ax = a.x + a.width / 2;
-  const ay = a.y + a.height / 2;
-  const bx = b.x + b.width / 2;
-  const by = b.y + b.height / 2;
-
-  const dx = bx - ax;
-  const dy = by - ay;
-
-  function intersect(cx: number, cy: number, w: number, h: number, dx: number, dy: number) {
-    const t = Math.min(
-      dx === 0 ? Infinity : (w / 2) / Math.abs(dx),
-      dy === 0 ? Infinity : (h / 2) / Math.abs(dy)
-    );
-
-    return {
-      x: cx + dx * t,
-      y: cy + dy * t,
-    };
-  }
-
-  return {
-    from: intersect(ax, ay, a.width, a.height, dx, dy),
-    to: intersect(bx, by, b.width, b.height, -dx, -dy),
-  };
 }
